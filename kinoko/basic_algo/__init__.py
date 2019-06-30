0635c1dcb977c713bc90a915b2008a2be2b681f4
QIAN Weishuo <koyo922@qq.com>
2019-06-30 22:42:39 +0800

    reverseList()

---
+++ kinoko/basic_algo/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 expandtab number
+"""
+basic data structure and algorithm for reference
+
+Authors: qianweishuo<qzy922@gmail.com>
+Date:    2019/6/30 下午10:02
+"""
+
