2728319118f59f78ffd5f0abd91becef58fca9a6
QIAN Weishuo <koyo922@qq.com>
2019-09-29 18:02:47 +0800

    ADD: better coverage for misc.network

--- tests/test_network.py
+++ tests/test_network.py
@@ -8,14 +8,22 @@ Date:    2019/6/27 下午11:20
 from __future__ import unicode_literals
 
 import os
+from typing import Text
 
 import pytest
+import requests
+import six
 
+from kinoko.misc.network.chase_redirection import (chase_url_cli, chase_redirection, ChaseError,
+                                                   UnexpectedHttpStatus, DepthOverflow)
 from kinoko.misc.network.proxy import proxing, DEFAULT_HTTP_PROXY
-from kinoko.misc.network.reachability import JINGDONG_REACHABLE
 from kinoko.text.io import file_wrapper
 
-from kinoko.misc.network.chase_redirection import chase_url_cli, chase_redirection, ChaseError
+if six.PY3:
+    from unittest.mock import Mock
+else:
+    # noinspection PyPackageRequirements
+    from mock import Mock
 
 url_jumps = [
     ('http://www.cyberciti.biz/tips/', ['http://www.cyberciti.biz/tips/', 'https://www.cyberciti.biz/tips/']),
@@ -23,17 +31,48 @@ url_jumps = [
     ('http://nonexist.site', [])
 ]
 
-pytestmark = pytest.mark.skipif(not JINGDONG_REACHABLE, reason='not internet available')
+
+# noinspection PyUnusedLocal
+def mock_head(url, timeout=None):
+    if url == 'http://www.cyberciti.biz/tips/':
+        return Mock(status_code=requests.codes.found, headers={'location': 'https://www.cyberciti.biz/tips/'})
+    elif url == 'https://www.cyberciti.biz/tips/':
+        return Mock(status_code=requests.codes.ok)
+    elif url == 'http://apollo.auto/index_cn.html':
+        return Mock(status_code=requests.codes.ok)
+    elif url == 'http://nonexist.site':
+        raise requests.exceptions.ConnectionError('mocked unreachable url')
+    else:
+        raise ValueError('unexpected url: ' + url)
 
 
 @pytest.mark.parametrize("url, jumps", url_jumps)
-def test_chase_redirection(url, jumps):
+def test_chase_redirection(url, jumps, mocker):
     """ 测试chase_redirection的逻辑，包括异常 """
+    mocker.patch.object(requests, 'head', mock_head)
+
     try:
         all_jumps = chase_redirection(url, max_depth=3)
     except ChaseError:
         all_jumps = []
     assert jumps == all_jumps
+
+
+def test_chase_redirection_exceptions(mocker):
+    mocker.patch.object(requests, 'head', return_value=Mock(status_code=911))
+    with pytest.raises(UnexpectedHttpStatus) as ex:
+        chase_redirection('dummy_url', max_depth=3)
+        assert ex.http_status == 911
+
+    # noinspection PyUnusedLocal
+    def mock_head_10(url, timeout=None):
+        back_url = int(url) + 1 if int(url) < 10 else 10
+        return Mock(status_code=requests.codes.found, headers={'location': Text(back_url)})
+
+    mocker.patch.object(requests, 'head', side_effect=mock_head_10)
+    with pytest.raises(DepthOverflow) as ex:
+        chase_redirection('1', max_depth=5)
+        assert ex.url == '5'
 
 
 def test_chase_url_cli(tmp_path):
